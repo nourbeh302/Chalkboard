ca935b2177bec3f0be020140c322ca9fa900cef3
Nour Eldin Samir <nourelddine.sam302@gmail.com>
2024-05-10 20:41:15 +0300

    Update: Add user services

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import {
+  Auth,
+  AuthModule,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private firestore = inject(Firestore);
+  private firebaseAuth = inject(Auth);
 
-  constructor() { }
+  constructor() {}
+
+  async login(email: string, password: string) {
+    const credential = await signInWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    );
+
+    return credential.user;
+  }
+
+  async register(email: string, password: string) {
+    await createUserWithEmailAndPassword(this.firebaseAuth, email, password);
+
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+    await addDoc(collection(this.firestore, 'Users'), userData);
+  }
+
+  async logOut() {
+    await signOut(this.firebaseAuth)
+  }
 }
